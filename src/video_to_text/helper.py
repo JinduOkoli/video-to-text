e5095587510100a8ff7f488faf198d64586f212f
Chijindu Okoli <chijinduokoli@Chijindus-MBP.attlocal.net>
2025-08-31 21:05:37 -0500

    Add date range filtering

---
+++ src/video_to_text/helper.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timezone
+import isodate
+
+def convert_iso_to_datetime(iso_str: str) -> datetime:
+    """
+    Convert ISO 8601 string to datetime.
+    If UTC, assume naive.
+    """
+    dt = isodate.parse_datetime(iso_str)
+    if dt.tzinfo:
+        dt = dt.replace(tzinfo=None)
+    return dt
